99e7cac04f5ab19a70d1461a3b2616f4703792b6
Stanislav-Kovalchuk <147875939+Stasnislav-Kovalchuk@users.noreply.github.com>
2026-03-16 23:12:37 +0200

    finished
    finished

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
-import '../widgets/custom_button.dart';
+// import '../widgets/custom_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
